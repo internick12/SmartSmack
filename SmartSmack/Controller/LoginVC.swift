78185c99c3d872339ff2cb4bb2b747cea6507579
Jose Trinidad Garcia <internick12@gmail.com>
2019-04-26 00:08:55 -0500

    with commit Login ui design

---
+++ SmartSmack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  SmartSmack
+//
+//  Created by Trinidad Garcia on 4/25/19.
+//  Copyright © 2019 Trinidad Garcia. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ SmartSmack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  SmartSmack
+//
+//  Created by Trinidad Garcia on 4/25/19.
+//  Copyright © 2019 Trinidad Garcia. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
