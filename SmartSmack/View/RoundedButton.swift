6022cb967e3f7d2670cdb5d4aec9f9cc929cede6
Jose Trinidad Garcia <internick12@gmail.com>
2019-05-14 00:48:42 -0500

    Add register account and boded raise for buttons

---
+++ SmartSmack/View/RoundedButton.swift
@@ -0,0 +1,33 @@
+//
+//  RoundedButton.swift
+//  SmartSmack
+//
+//  Created by Trinidad Garcia on 5/14/19.
+//  Copyright © 2019 Trinidad Garcia. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+
+}
